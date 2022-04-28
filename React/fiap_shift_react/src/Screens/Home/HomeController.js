8da08778cdf9129278f8ed800d7dd7c49abd56d1
Giovanna Menegoli <giovanna.menegoli@hotmail.com>
2022-04-27 22:11:11 -0300

    Aula 02 React

--- React/fiap_shift_react/src/Screens/Home/HomeController.js
+++ React/fiap_shift_react/src/Screens/Home/HomeController.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import HomeView from './HomeView'
-import HomeModel from './HomeModel'
 
-class HomeController extends React.Component {
+import React, { Component } from 'react';
+import HomeView from './HomeView'
+
+class HomeController extends Component {
 
     constructor() {
         super();
-        this.homeModel = new HomeModel();//inicializando o view    
-        this.homeModel.getSomeInfo();
         this.state = {
-            count: 0
+            count: 0,
+            count1: 0,
+            estaCarregando: false,
         };
 
-        // setInterval(() => {
-        //     this.setState({
-        //         count: this.state.count + 1
-        //     })
-        // }, 1000);
+        setInterval(() => {
+            this.setState({
+                count: this.state.count + 1
+            })
+        }, 1000);
     }
 
 
-    clickButton = () => {
+    addCount = () =>  {
         this.setState({
-            count: this.state.count + 1
+            count1: this.state.count1+1
         })
+    }
+
+    iniciaLogin = () => {
+        // if(this.state.estaCarregando){
+        //     this.setState({
+        //         estaCarregando: false
+        //     })
+        // } else {
+        //     this.setState({
+        //         estaCarregando: true
+        //     })
+        // }
+        this.setState({
+            estaCarregando: !this.state.estaCarregando
+        });
     }
 
     render() {
         return (
-            <HomeView count={this.state.count} clickButton={this.clickButton} /> //Chamando o View
+            <HomeView 
+                count={this.state.count} 
+                count1={this.state.count1} 
+                addCount={this.addCount}
+                estaCarregando={this.state.estaCarregando}
+                iniciaLogin={this.iniciaLogin}
+            /> //Chamando o View
         )
     }
 }
--- React/fiap_shift_react/src/index.js
+++ React/fiap_shift_react/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeController from './Screens/Home/HomeController';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MuiThemes from './Utils/Common/MuiThemes';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <HomeController/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <MuiThemes>
+      <HomeController />
+    </MuiThemes>    
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
